7252b426e3b4dc6c5e04b980151d4701b72bfbe4
Jim Frode Hoff <jimfrodehoff@gmail.com>
2014-10-14 22:45:59 +0200

    begin stopwatch

--- oving8/stopwatch.js
+++ oving8/stopwatch.js
@@ -1,3 +1,15 @@
 /**
  * Created by jimtrim on 14/10/14.
  */
+
+
+var StopWatch = {
+	updateClock: function (startTime) {
+		var outTime = new Date().getTime() - startTime;
+		document.getElementById("display-area").textContent = outTime;
+	}
+}
+addEventListener("load", function() {
+	var now = new Date().getTime();
+	StopWatch.updateClock(now - 36000);
+});
