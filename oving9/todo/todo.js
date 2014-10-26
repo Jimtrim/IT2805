7e0fd3c0a8b236e88af25ae02918b28b9380cf6c
Jim Frode Hoff <jimfrodehoff@gmail.com>
2014-10-26 17:18:17 +0100

    adding element working, no validation tho

--- oving9/todo/todo.js
+++ oving9/todo/todo.js
@@ -1,3 +1,27 @@
 /**
  * Created by jimtrim on 26/10/14.
  */
+
+var Todo = Todo || {};
+Todo.addTask = function () {
+	var taskText = document.getElementById("add-task-text" ).value;
+	document.getElementById("add-task-text" ).value = "";
+	console.log(taskText);
+
+	document.getElementById("todo-list" ).appendChild(Todo.buildListElement(taskText));
+};
+
+Todo.buildListElement = function (text) {
+	var li = document.createElement("li");
+	li.innerHTML = '<label><input type="checkbox" name="task-done">' + text + '</label>';
+	return li;
+};
+
+Todo.clearCompletedElements = function() {
+
+};
+
+addEventListener("load", function() {
+	document.getElementById("add-task-button").addEventListener("click", Todo.addTask);
+	document.getElementById("clear-completed-button").addEventListener("click", Todo.addTask);
+});
